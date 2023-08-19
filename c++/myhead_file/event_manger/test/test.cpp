47355d014dd7d62a69c07ae6d1e91ee5d231185e
luoyebai <2112216825@qq.com>
2023-08-19 11:21:06 +0800

    修改了log宏,防止冲突

--- c++/myhead_file/event_manger/test/test.cpp
+++ c++/myhead_file/event_manger/test/test.cpp
@@ -1,19 +1,31 @@
+/**
+ * @file test.cpp
+ * @author luoyebai (2112216825@qq.com)
+ * @brief 测试
+ * @version 0.1
+ * @date 2023-08-11
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
 // 关闭日志颜色
 // #define CLOSE_COLOR
 
 #include "test.hpp"
-
-using namespace std::literals;
+// 时间
+// using namespace std::literals;
+// sleep(1s);
 
 int main(int argc, char *argv[]) {
+    logSystemInit("洛叶白", "../../../../log/", KUNKNOW);
     logTest();
-    // 异步试
+    // 异步测试
     // exit(0);
-    logi("按回车退出程序");
-    logi("123333");
-    logi("7777777777777777777777777");
+    log_i("按回车退出程序");
+    log_i("测试");
+    log_i("再次测试");
     std::thread(pubTest).detach();
     std::thread(subTest).detach();
     getchar();
-    // sleep(1s);
 }
--- c++/myhead_file/event_manger/test/test.hpp
+++ c++/myhead_file/event_manger/test/test.hpp
@@ -1,28 +1,37 @@
 #ifndef INCLUDE_TEST_HPP
 #define INCLUDE_TEST_HPP
 
+#include "log/log.hpp"
 // std
 #include <string>
 // include
 #include "event_manger/publisher.hpp"
 #include "event_manger/subscriber.hpp"
-#include "log/log.hpp"
 
+/**
+ * @brief
+ * 这是日志测试函数
+ */
 void logTest() {
-    // 看不到debug
-    logSystemInit("洛叶白", "../../../../log/", KDEBUG);
+    // 初始化日志系统
+    // logSystemInit("洛叶白", "../../../../log/", KUNKNOW);
     // log_system->log_mode[1] = 0;
     // log_system->log_mode[2] = 0;
-    logDebug("日志系统初始化完毕");
-    logUnknow("这是未知信息");
-    logDebug("这是调试信息");
-    logInfo("这是正常信息");
-    logWarn("这是警告信息");
-    logError("这是错误信息");
+    log_d("日志系统初始化完毕");
+
+    log_u("这是未知信息");
+    log_d("这是调试信息");
+    log_i("这是正常信息");
+    log_w("这是警告信息");
+    log_e("这是错误信息");
     // Fatal等级报错同时退出程序
-    // logFatal("这是失败信息");
+    // log_f("这是失败信息");
 }
 
+/**
+ * @brief
+ * 这是发布者测试函数
+ */
 void pubTest() {
     auto pub =
         Publisher<std::string>::makeSharePtr("发布者1号", "话题测试", 10);
@@ -42,7 +51,7 @@ void pubTest() {
             static size_t i = 0;
             auto data = str[i];
             pub->push(data);
-            // logInfo(pub->getLogger(), "发布数据:", '{', data, '}');
+            // log_i(pub->getLogger(), "发布数据:", '{', data, '}');
             ++i;
             if (i >= 10) {
                 // 跳出callback
@@ -56,7 +65,7 @@ void pubTest() {
     // 订阅者回调
     sub->timerCallBack([&sub] {
         auto data = sub->pop();
-        // logInfo(sub->getLogger(), "接收数据:", '{', data, '}');
+        // log_i(sub->getLogger(), "接收数据:", '{', data, '}');
         if (data == "杀死订阅者") {
             return false;
         }
@@ -64,6 +73,10 @@ void pubTest() {
     });
 }
 
+/**
+ * @brief
+ * 这是订阅者测试函数
+ */
 void subTest() {
     auto pub =
         Publisher<std::string>::makeSharePtr("发布者2号", "另一个话题", 10);
@@ -72,9 +85,9 @@ void subTest() {
     sub->timerCallBack([&sub, &pub] {
         // 取出订阅者1号,让发布者2号发布
         auto data = sub->pop();
-        // logInfo(sub->getLogger(), "接收数据:", '{', data, '}');
+        // log_i(sub->getLogger(), "接收数据:", '{', data, '}');
         pub->push(data);
-        // logInfo(pub->getLogger(), "发布数据:", '{', data, '}');
+        // log_i(pub->getLogger(), "发布数据:", '{', data, '}');
         if (data == "杀死订阅者") {
             // 此时跳出,发布者2号也会停止发送
             return false;
